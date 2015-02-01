35123b4670d8e0263a4a928f776d5ccfc5258585
Tom <tom@noogen.net>
2015-02-01 01:54:53 -0600

    Update gsn_install_common.sh

--- wp-setup/bin/lib/gsn_install_common.sh
+++ wp-setup/bin/lib/gsn_install_common.sh
@@ -4,5 +4,5 @@ function gsn_install_common()
 {
   gsn_lib_echo "Installing Common Library, please wait..."
   apt-get -y --force-yes install nfs-common wget mysql-client nginx pwgen python-setuptools curl unzip \
-    libssh2-php haproxy 2>&1 || gsn_lib_error "Unable to install Common Library, exit status = " $?
+    libssh2-php 2>&1 || gsn_lib_error "Unable to install Common Library, exit status = " $?
 }
