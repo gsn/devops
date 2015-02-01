b0e4dffd3b8dc3039345fca3b930000dccaf5d77
Tom <tom@noogen.net>
2015-01-31 21:09:13 -0600

    Update gsn_install_adminer.sh

--- wp-setup/bin/lib/gsn_install_adminer.sh
+++ wp-setup/bin/lib/gsn_install_adminer.sh
@@ -2,15 +2,15 @@
 
 function gsn_install_adminer()
 {
-  if [ ! -d /mnt/sharefs/admin/db/adminer ]; then
+  if [ ! -d /mnt/sharefs/admin/htdocs/db/adminer ]; then
 
     # Setup Adminer
-    mkdir -p /mnt/sharefs/admin/db/adminer/ \
-    || gsn_lib_error "Unable to create Adminer directory: /mnt/sharefs/admin/db/adminer/, exit status = " $?
+    mkdir -p /mnt/sharefs/admin/htdocs/db/adminer/ \
+    || gsn_lib_error "Unable to create Adminer directory: /mnt/sharefs/admin/htdocs/db/adminer/, exit status = " $?
 
     # Download Adminer
     gsn_lib_echo "Setup adminer"
-    cp ../config/adminer-4.1.0-mysql-en.php /var/www/admin/db/adminer/index.php
+    cp ../config/adminer-4.1.0-mysql-en.php /var/www/admin/htdocs/db/adminer/index.php
 
   fi
 }
